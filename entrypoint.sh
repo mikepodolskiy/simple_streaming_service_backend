bd6066dbcbb0d9a02d8c35a3b18d3c24ebf40a77
mikepodolskiy <mikepodolskiy@gmail.com>
2023-05-04 16:43:09 +0300

    test commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 export FLASK_APP=main.py
-gunicorn main:app -b 0.0.0.0:25000
-#flask run -h 0.0.0.0 -p 25000
+#gunicorn main:app -b 0.0.0.0:25000
+python -m flask db init
+python -m flask run -h 0.0.0.0 -p 25000
